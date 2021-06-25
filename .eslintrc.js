0689e0ebf00baa4a7b88a75ee414011722fdf855
Patrick Organ <patinthehat@users.noreply.github.com>
2021-06-25 12:16:01 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+    env: {
+        node: true,
+        browser: false,
+        commonjs: true,
+    },
+    settings: {},
+    extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'eslint:recommended',
+        'prettier',
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        //'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+        indent: ['error', 4, { SwitchCase: 1 }],
+    },
+};
