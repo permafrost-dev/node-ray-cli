57174f31da9b0bf3e32d0e813ec6984e56d98aeb
Patrick Organ <patrick@permafrost.dev>
2021-06-25 16:40:08 -0400

    wip

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,9 +1,5 @@
-import { add } from '../src/index';
+/* eslint-disable no-undef */
 
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
-});
-
-test('adding two plus two is four', () => {
-    expect(add(2, 2)).toBe(4);
 });
