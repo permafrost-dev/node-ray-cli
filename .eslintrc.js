c27381af90cc9a99986f29c09c175afe5c655963
Patrick Organ <patrick@permafrost.dev>
2021-06-27 05:03:46 -0400

    disable ban-ts-comment rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,12 @@ module.exports = {
         commonjs: true,
     },
     settings: {},
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'eslint:recommended',
-        //'prettier',
-        //'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ],
+    extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
         indent: ['error', 4, { SwitchCase: 1 }],
